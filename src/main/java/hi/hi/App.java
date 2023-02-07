9b02fdb6598a211210a1dbd00329b24dfdd0fb51
mahendra <asdq1740@gmail.com>
2023-02-07 15:06:33 +0530

    "hvhjhjk"

---
+++ src/main/java/hi/hi/App.java
@@ -0,0 +1,24 @@
+package hi.hi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+int a,b;
+public int addition(int a,int b) {
+	return a+b;
+}
+public int subtraction(int a,int b) {
+	return a-b;
+}
+public int multiplication(int a,int b) {
+	return a*b;
+}
+public int division(int a,int b) {
+	return a/b;
+}
+
+
+}
---
+++ src/test/java/hi/hi/CalculatorTest.java
@@ -0,0 +1,35 @@
+package hi.hi;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	
+	    public static App objCalculator;
+		@Before
+		public void setUp() throws Exception {
+			objCalculator=new App();
+		}
+		@After
+	    public void tearDown() throws Exception{
+	    	System.out.println("Closing");
+	    }
+		@Test
+		public void additionTest() {
+			assertEquals(25,objCalculator.addition(12,13));
+		}
+		public void subtractionTest() {
+			assertEquals(-1,objCalculator.subtraction(12,13));
+		}
+		public void multiplicationTest() {
+			assertEquals(156,objCalculator.multiplication(12,13));
+		}
+		public void divisionTest() {
+			assertEquals(0,objCalculator.division(12,13));
+		}
+
+	}
